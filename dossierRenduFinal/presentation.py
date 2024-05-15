c8e7a3f89a4fb9848750ae433cd45c969b87a6c5
AntonyLeclerc <ptibaka@DESKTOP-3GSMA1J.localdomain>
2024-05-15 22:16:44 +0200

    Ajout rapport

--- dossierRenduFinal/presentation.py
+++ dossierRenduFinal/presentation.py
@@ -8,7 +8,21 @@ import tensorflow as tf
 #model = tf.keras.models.load_model("dossierRenduFinal/MP/noyau5x5_10epochs_images70x70_flip_shift_preprocessed.h5")
 
 # Pour la présentation
-model = tf.keras.models.load_model("./noyau5x5_5epochs/noyau5x5_5epochs.h5")
+model = tf.keras.models.load_model("./noyau5x5_20epochs/noyau5x5_20epochs.h5")
+
+
+""" model = tf.keras.models.Sequential()
+model.add(tf.keras.layers.Conv2D(32, (5, 5), activation='relu', input_shape=(70, 70, 3)))
+model.add(tf.keras.layers.MaxPooling2D((2, 2)))
+model.add(tf.keras.layers.Conv2D(64, (5, 5), activation='relu'))
+model.add(tf.keras.layers.MaxPooling2D((2, 2)))
+model.add(tf.keras.layers.Conv2D(64, (5, 5), activation='relu'))
+model.add(tf.keras.layers.MaxPooling2D((2, 2)))
+model.add(tf.keras.layers.Flatten())
+model.add(tf.keras.layers.Dense(128, activation='relu'))
+model.add(tf.keras.layers.Dense(64, activation='relu'))
+model.add(tf.keras.layers.Dense(51, activation='softmax'))  
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy']) """
 
 class DragDropImageApp:
     def __init__(self, root):
